f4cd3815e9884f91bcd18b72f49db47dc384e0c1
Darren <ninenumbproductions@gmail.com>
2018-04-02 12:20:37 -0700

    added check for string and number validation

--- functiondrills.js
+++ functiondrills.js
@@ -3,13 +3,19 @@ function whoAmI (name, age) {
 
   if (!name || !age) {
     throw new Error('can not have empty field');
+  }    
+
+  if (typeof name !== 'string' || typeof age !== 'number') {
+    throw new Error('name must be text/ age must be number');
   }
-    
+
   console.log(`Hi, my name is ${name} and I'm ${age} years old`);
   console.log(`I was born in ${age}`);
 }
 
-whoAmI('', 29);
+
+
+whoAmI('Steve',"29");
 
 function yearOfBirth(age) {
   if ( age < 0) {
