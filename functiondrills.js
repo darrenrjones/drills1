d386ad96f1474980d19419ad6a5a8efc62ac6647
Darren <ninenumbproductions@gmail.com>
2018-04-02 12:08:12 -0700

    yearOfBirth function

--- functiondrills.js
+++ functiondrills.js
@@ -1,10 +1,15 @@
 'use strict';
-function whoAmI (name, age) {
-  let yearOfBirth = 2018 - age;
+function whoAmI (name, age) {  
+  
   console.log(`Hi, my name is ${name} and I'm ${age} years old`);
-  console.log(`I was born in ${yearOfBirth}`);
+  console.log(`I was born in ${age}`);
 }
 
 whoAmI('Chris', 29);
 
+function yearOfBirth(age) {
+  let yob = 2018 - age;
+  console.log(yob);
+}
 
+yearOfBirth(29);
