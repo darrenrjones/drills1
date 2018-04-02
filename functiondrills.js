2abf876c12e3698e7a1000a513ddb3f3ef9b2388
Darren <ninenumbproductions@gmail.com>
2018-04-02 12:17:43 -0700

    added catch for blank field

--- functiondrills.js
+++ functiondrills.js
@@ -1,11 +1,15 @@
 'use strict';
 function whoAmI (name, age) {  
-  
+
+  if (!name || !age) {
+    throw new Error('can not have empty field');
+  }
+    
   console.log(`Hi, my name is ${name} and I'm ${age} years old`);
   console.log(`I was born in ${age}`);
 }
 
-whoAmI('Chris', 29);
+whoAmI('', 29);
 
 function yearOfBirth(age) {
   if ( age < 0) {
