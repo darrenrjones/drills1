ddf92f72f037210c5d38eb9370df2423c200d380
Katharine McLeod <katharinermcleod@gmail.com>
2018-04-02 14:57:01 -0400

    created yearOfBirth

--- functiondrills.js
+++ functiondrills.js
@@ -1,6 +1,10 @@
-"use strict"
+'use strict';
 function whoAmI (name, age) {
+  let yearOfBirth = 2018 - age;
   console.log(`Hi, my name is ${name} and I'm ${age} years old`);
+  console.log(`I was born in ${yearOfBirth}`);
 }
 
-whoAmI("Chris", "29")
+whoAmI('Chris', 29);
+
+
